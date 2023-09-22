d9aed66fc892f57a848987fed8b97153605db092
0Hasan0 <hr145310@gmail.com>
2023-09-22 20:33:01 +0300

    fix pages error

--- app.js
+++ app.js
@@ -1,3 +1,123 @@
+class Player {
+    constructor(ctx, r, x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = r;
+        // hsl(${ Math.random() * 360 }, 100 %, 40 %)
+        this.color = 'white';
+        this.ctx = ctx;
+    }
+
+    draw(ctx = this.ctx) {
+        ctx.fillStyle = this.color;
+        ctx.shadowBlur = 15;
+        ctx.shadowColor = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.shadowBlur = 0;
+    }
+
+    move(dir, speed) {
+        if (dir === 'up') {
+            this.y -= speed;
+        } else if (dir === 'down') {
+            this.y += speed;
+        } else if (dir === 'left') {
+            this.x -= speed;
+        } else if (dir === 'right') {
+            this.x += speed;
+        }
+    }
+}
+
+class Bullet {
+    constructor(x, y, r, ctx, angle) {
+        this.x = x;
+        this.y = y;
+        this.radius = r / 5; // Adjust the bullet radius as needed
+        this.ctx = ctx;
+        this.speed = 5; // Adjust the bullet speed as needed
+        this.angle = angle;
+        this.color = "red"; // Adjust the bullet color as needed
+    }
+
+    move() {
+        this.x += this.speed * Math.cos(this.angle);
+        this.y += this.speed * Math.sin(this.angle);
+    }
+
+    draw(ctx = this.ctx) {
+        ctx.shadowBlur = 15;
+        ctx.shadowColor = this.color;
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+class Cannon {
+    constructor(x, y, ctx, r, angle, shootingInterval) {
+        this.x = x;
+        this.y = y;
+        this.radius = r;
+        this.angle = angle;
+        this.ctx = ctx;
+        this.cannonColor = "white";
+        this.shootingInterval = shootingInterval;
+        this.minShootingInterval = 500;
+        this.maxShootingInterval = 2000;
+        this.randomizeShootingTime()
+        this.lastShotTime = Date.now();
+        this.bullets = [];
+    }
+    randomizeShootingTime() {
+        this.shootingInterval = Math.random() * (this.maxShootingInterval - this.minShootingInterval) + this.minShootingInterval;
+    }
+
+    draw(ctx = this.ctx) {
+        ctx.shadowBlur = 15;
+        ctx.shadowColor = this.cannonColor;
+        ctx.fillStyle = this.cannonColor;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = 'black';
+        ctx.shadowColor = this.cannonColor;
+        ctx.arc(this.x, this.y, this.radius / 1.5, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = this.cannonColor;
+        ctx.shadowColor = this.cannonColor;
+        // cannon barrel with angle
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.fillRect(this.radius / 1.2, -this.radius / 4, this.radius * 1.2, this.radius / 2);
+        ctx.rotate(-this.angle);
+        ctx.translate(-this.x, -this.y);
+        ctx.shadowBlur = 0;
+    }
+
+    shoot(playerX, playerY) {
+        if (Date.now() - this.lastShotTime > this.shootingInterval) {
+            const angle = Math.atan2(playerY - this.y, playerX - this.x);
+            const bullet = new Bullet(this.x, this.y, this.radius, this.ctx, angle);
+            this.bullets.push(bullet);
+            this.lastShotTime = Date.now();
+            this.randomizeShootingTime();
+        }
+    }
+
+    updateBullets() {
+        this.bullets.forEach(bullet => {
+            bullet.move();
+            bullet.draw();
+        });
+    }
+}
+
 
 const start_btn = document.getElementById('start_btn');
 const Game = {
